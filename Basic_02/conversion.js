9461b35402d020c8b71efcab8be5bb25509eb54f
Pranjal Jaiswal <pranjaljaiswal35@gmail.com>
2024-02-02 13:19:09 +0530

    feat:add new file

---
+++ Basic_02/conversion.js
@@ -0,0 +1,25 @@
+const name = "Pranjal";
+console.log(typeof name);
+console.log(typeof(name))
+
+let nameinNumber = Number(name);
+
+console.log(nameinNumber, typeof nameinNumber);
+
+//"33"=>33
+//"44pj"=>NaN
+//true=>1 , false=>0
+//3*"3"=>9
+//4*[2]=>8
+//4+[2]=>"42"
+//4*[1,2]=>NaN
+//NaN==NaN=>false
+
+let nameInBoolean = Boolean(name);
+console.log(nameInBoolean);
+
+console.log(typeof(4+[2]),4*[1,2],NaN===NaN)
+
+
+
+
